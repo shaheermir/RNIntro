45d9bdab91e5a9671f80f5abd93dea52aabd8aac
Shaheer Mir <shaheermir0@gmail.com>
2018-02-10 20:44:38 -0500

    Added Card, CardSection, and Button components.

--- Albums/src/components/AlbumDetail.js
+++ Albums/src/components/AlbumDetail.js
@@ -1,13 +1,65 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import PropTypes from 'prop-types'
+import { View, Text, Image, StyleSheet } from 'react-native'
+
+import Card from './Card'
+import CardSection from './CardSection'
+import Button from './Button'
 
 const AlbumDetail = props => {
-  const { album } = props
+  const { title, artist, thumbnail_image: thumbnailImage, image } = props.album
+  const { headerContentStyle, thumbnailStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles
+
   return (
-    <View>
-      <Text>{album.title}</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnailImage }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image source={{ uri: image }} style={imageStyle} />
+      </CardSection>
+
+      <CardSection>
+        <Button />
+      </CardSection>
+    </Card>
   )
+}
+
+const styles = StyleSheet.create({
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    width: 60,
+    height: 60
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1
+    // width: null
+  }
+})
+
+AlbumDetail.propTypes = {
+  album: PropTypes.object.isRequired
 }
 
 export default AlbumDetail
