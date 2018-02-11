07e65252429fc0dec55537e461e79a1dd7008c79
Shaheer Mir <shaheermir0gmail.com>
2018-02-10 20:44:38 -0500

    Added Card, CardSection, and Button components.

---
+++ Albums/src/components/CardSection.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import PropTypes from 'prop-types'
+
+const CardSection = props => {
+  return <View style={styles.containerStyles}>{props.children}</View>
+}
+
+const styles = StyleSheet.create({
+  containerStyles: {
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+    padding: 5,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    position: 'relative'
+  }
+})
+
+CardSection.propTypes = {
+  children: PropTypes.object
+}
+
+export default CardSection
