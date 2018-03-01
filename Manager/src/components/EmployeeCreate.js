f37a4656dd5e69e9663a98f45d4565717cd6332b
Shaheer Mir <shaheermir0@gmail.com>
2018-02-28 20:37:21 -0500

    Added EmployeeList and EmployeeCreate screens

---
+++ Manager/src/components/EmployeeCreate.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class EmployeeCreate extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text> employeee form</Text>
+      </View>
+    )
+  }
+}
+
+export default EmployeeCreate
---
+++ Manager/src/components/EmployeeList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class EmployeeList extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>EmployeelIst</Text>
+        <Text>EmployeelIst</Text>
+        <Text>EmployeelIst</Text>
+        <Text>EmployeelIst</Text>
+        <Text>EmployeelIst</Text>
+        <Text>EmployeelIst</Text>
+        <Text>EmployeelIst</Text>
+      </View>
+    )
+  }
+}
+
+export default EmployeeList
